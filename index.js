a03cbe191c9ced103cd35fb87ba329de37b02b3d
Ashley Robinson <organiccircuits@gmail.com>
2020-04-24 16:37:21 -0400

    Code debug, cleaned up comments.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const moment = require('moment');
 
 //Title: threeSixty
-//Description: This small program calculates matching clock angles for
-//cities in different timezones. This version calculates those between EST and
-//PST with future versions moving away from hardcoded values with the ability
-//to calculate matching angles between cities
+//Description: This program calculates matching clock angles for cities
+//in different timezones. This version calculates those between EST and
+//PST with future versions moving away from hardcoded values and the
+//ability to calculate matching angles between cities
 //Author: Ashley Robinson
+
   async function calculateTheta(time) {
   //moment.js to return current time in HH:MM format
   let eastCoastTime = moment.format('LT');
@@ -42,53 +43,44 @@ const moment = require('moment');
     matchingAngleArray.push(time);
   }
   return matchingAngleArray;
-}
+}});
 
+//main function that calls calculateTheta() that compares angles
+//formed by the hour and minute hand
 (function getMatchingAngles() {
 
-  const matchingAngleArray = [];
-  //for all times in a 24 hour window using moment
+  //24 hour window using moment to include matching am and pm values
   const aroundTheClock = [moment().moment.endOf('day').fromNow()];
   const matchingAngles = aroundTheClock.map(time =>{
     calculateTheta(time);
   });
-
   return matchingAngles;
   })();
 
-//assign hour angles in a clock to correspond to increments of 30^o
-//in future, have degrees in a circle divided by hours
-const clockHours = [0,1,2,3,4,5,6,7,8,9,10,11,12];
-const hourClockAngles = [];
-const minuteClockAngles = [];
-//TODO read span operator with ..60 or varargs (preferable)
-const clockMinutes = [0,20,40,60];
-//this can be improved by adding an if-else statement
-//that added 30^o if it was an hour hand, and 6^o if
-//a minute hand and pushed to the appropriate array
+//assign clock angles to increment by 30 degrees for hours and 6 for minutes
+const clockHours = [...Array(12).keys()];
+const clockMinutes = [...Array(60).keys()];
+//simulate dictionary for calculateTheta() to retrieve value for hoour/minute
+let hourAnglePair = {};
+let minuteAnglePair = {};
+
 const hourAngles = clockHours.map(hour => {
-  //TODO test this outside of both aarrays
-  let currAngle;
-  //returning a JavaScript Object, not JSON so:
-  let hourAngleJSO;
-  currAngle += 30;
+  let currentAngle;
+  currentAngle += 30;
   console.log(currAngle);
-  hourAngleJSO = {"hour" : "currAngle"};
-  hourClockAngles.push({hourAngleJSO});
-  console.log(hourClockAngles);
-  return hourClockAngles;
-});
+  hourAnglePair.push(...hour, ...currentAngle);
+  });
+  //check to see what the array contains
+  console.log(hourAngles);
 
 const minuteAngles = clockMinutes.map(minute => {
-  let currAngle;
-  currAngle += 6;
+  let currentAngle;
+  currentAngle += 6;
   console.log(currAngle);
-  minuteAngleJSO = {"minute" : "currAngle"};
-  minuteClockAngles.push(minuteAngleJSO);
-  console.log(minuteClockAngles);
-  return minuteClockAngles;
-});
-
+  minuteAnglePair.push(...minute, ...currentAngle);
+  });
+  //check to see what the array contains
+  console.log(minuteAngles);
 
 
 
