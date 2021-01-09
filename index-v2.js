49702ef3260b0397640678f0467e62fd67b5b3da
organiccircuits <organiccircuits@gmail.com>
2021-01-08 19:28:36 -0500

    updated to es6 arrow functions, added a .gitignore file for node_modules.

---
+++ index-v2.js
@@ -0,0 +1,218 @@
+const moment = require("moment");
+
+//Title: threeSixty
+//Description: This program calculates matching clock angles for cities
+//in different timezones. This version calculates those between EST and
+//PST with future versions moving away from hardcoded values and the
+//ability to calculate matching angles between any two timezones.
+//Author: Ashley Robinson
+
+//TODO add other time zones
+let utcConversion = (timezone) => {
+
+  //moment to return current time in HH:MM format
+  //TODO create military/civilian time conversion
+  const utcTime = moment.utc("2020-07-06T04:15:30Z");
+  let utcHour = utcTime.format("HH");
+  const minutes = utcTime.format("mm");
+  console.log(utcTime);
+  console.log(utcHour);
+  console.log(minutes);
+
+  switch (timezone) {
+    case "EST":
+    case "EDT":
+      //east coast time is 4 hours behind utc
+      //other timezones follow east to west
+      let eastCoastHour = utcHour - 4;
+      //moment will return 00, 01, etc times; checkIfNegative will return only
+      //positive values for the conversion and assign it to the zone's hour
+      let hour = checkIfNegative(eastCoastHour);
+      console.log(hour);
+      //construct the actual clock value
+      const eastCoastTime = hour + ":" +  minutes;
+      console.log(eastCoastTime);
+      return eastCoastTime;
+    case "CST":
+    case "CDT":
+      const centralHour = utcHour - 5;
+      let centralPositive = checkIfNegative(centralHour);
+      console.log(centralPositive);
+      const centralTime = centralPositive + ":" +  minutes;
+      console.log(centralTime);
+      return centralTime;
+    case "MST":
+    case "MDT":
+      const mountainHour = utcHour - 6;
+      let mountainPositive = checkIfNegative(mountainHour);
+      console.log(mountainPositive);
+      const mountainTime = mountainPositive + ":" +  minutes;
+      console.log(mountainTime);
+      return mountainTime;
+    case "PST":
+    case "PDT":
+      const pacificHour = utcHour - 7;
+      let pacificPositive = checkIfNegative(pacificHour);
+      console.log(pacificPositive);
+      const pacificTime = pacificPositive + ":" +  minutes;
+      console.log(pacificTime);
+      return pacificTime;
+    default:
+      const msg = "Eoth talich danya: No valid timezone entered.";
+      console.log(msg);
+      return msg;
+  }
+}
+
+//TODO learn shortcut for refactoring, selecting whole word
+//checks if the resulting conversion from utc resulted in a negative
+//number. If so, it adds 12 to correspond to actual clock values
+let  checkIfNegative = (hour) => {
+  if (hour <= 0) {
+    console.log("The hour was negative:", hour);
+    hour += 12;
+    return hour;
+  } else {
+    console.log("The hour was positive:", hour);
+    return hour;
+  }
+}
+
+//assigns each hand an agle with 90 degrees being 12 o'clock and 0 at 3 o'clock
+let assignClockDegrees = () => {
+  let hourHandDegrees = [];
+  let minuteHandDegrees = [];
+  let hour = 0;
+  let minute = 0;
+  let hourDegree = 0;
+  let minuteDegree = 0;
+
+  let calculateHours = () => {
+  do {
+    hourHandDegrees.push([hour, hourDegree]);
+    hourDegree += 30;
+    hour++;
+  } while (hourDegree <= 360);
+}
+
+  let calculateMinutes = () => {
+  do {
+    minuteHandDegrees.push([minute, minuteDegree]);
+    minuteDegree += 6;
+    minute++;
+  } while (minuteDegree <= 360) ;
+}
+  calculateHours();
+  calculateMinutes();
+  console.log("The clock hours and their angle is:", hourHandDegrees);
+  console.log("The clock minutes and their angle is:", minuteHandDegrees);
+
+  return hourHandDegrees, minuteHandDegrees;
+}
+
+//compares angles of two timezones in a 24 hour period and pushes matching
+//angles to an array that is returned to users
+let caluclateTheta = (hourHandOne, hourHandTwo, minuteHand) => {
+
+  //TODO check if assignClockDegrees and functions following need to be
+  //promises; if so, update accordingly. also see if this is best placement
+  assignClockDegrees();
+  //TODO compare time 12:15 AM EST (4:15 AM UTC) and 9:15 AM's angles
+  //TODO remove hardcoded values
+  //TODO return the second array value when the first matches hourHandOne, etc
+  const hourOneDegree = 0;
+  const hourTwoDegree = 0;
+  const minuteDegree = 90;
+
+  //each angle theta is calculated by subtracting the minuteDegree
+  //in degrees from each hour's assigned degree. if this results in a negative
+  //value, 360 is added to bring positive and still maintain the angle
+  let thetaOne = minuteDegree - hourOneDegree;
+  console.log(thetaOne);
+  let thetaTwo = minuteDegree - hourTwoDegree;
+ console.log(thetaTwo);
+
+    if (thetaOne < 0) {
+      thetaOne += 360;
+    }
+    if (thetaTwo < 0) {
+      thetaTwo += 360;
+    }
+
+  console.log("Angle one's value is:", thetaOne);
+  console.log("Angle two's value is:", thetaTwo);
+
+  let matchingAngleArray = [];
+
+  //if a match is found, push to an array
+  if (thetaOne == thetaTwo) {
+    matchingAngleArray.push(hourHandOne + ":" + minuteHand);
+  }else{
+    //TODO update console log to error message when aroundTheClock() is fxnal
+    console.log("The angles do no match.");
+  }
+
+  console.log(matchingAngleArray);
+  return matchingAngleArray;
+}
+
+//returns all matching angles between the two timezones for a 24 hour period
+let aroundTheClock = (hourHandOne, hourHandTwo, minuteHand) => {
+
+  //TODO ++ time by minute to run through clock +24 hourAngles;
+  //this will require hour incrementing every time minutes reach 60
+
+  //24 hour window using moment to include matching am and pm values
+  //TODO consider conversion to epoch time and increment in minutes (or s)
+    const utcTime = moment.utc("2020-07-06T04:15:30Z");
+    const utcEpoch = moment.utc("2020-07-06T04:15:30Z").unix();
+    const twentyFour = utcTime.add(24, 'hour');
+    const twentyFourEpoch = utcTime.add(24, 'hour').unix();
+    console.log(utcTime);
+    console.log(utcEpoch);
+    console.log(twentyFour);
+    console.log(twentyFourEpoch);
+
+    //for every minute in a 24 hour period
+    for (let i=0; utcEpoch < twentyFourEpoch; i++) {
+      caluclateTheta(hourHandOne, hourHandTwo, minuteHand);
+      minuteHand++;
+      if (minuteHand === 60) {
+        hourHandOne++;
+        hourHandTwo++;
+        minuteHand = 0;
+      }
+      let hourCounter = 0;
+      hourCounter++;
+      if (hourCounter >= 24) {
+        break;
+      }
+    }
+}
+
+//function to determine the two timezones the angles should be compared to
+let driver = (timezoneOne, timezoneTwo) => {
+
+  let timeOne = utcConversion(timezoneOne);
+  let timeTwo =  utcConversion(timezoneTwo);
+  console.log("Coverted timezone one", timeOne);
+  console.log("Coverted timezone two", timeTwo);
+
+  let hourOne = timeOne.substring(0,1);
+  let hourTwo = timeOne.substring(0,1);
+  //no need for both timeOne and timeTwo's minutes since the same
+  let minuteHand = timeOne.substring(3);
+  console.log("Hour substing one", hourOne);
+  console.log("Hour substing two", hourTwo);
+  console.log("Minute substring", minuteHand);
+
+
+  aroundTheClock(hourOne, hourTwo, minuteHand);
+
+}
+
+driver("EST", "PDT");
+
+// const matchingAngles = twentyFour.map(time => {
+//   return caluclateTheta(hourHandOne, hourHandTwo, minuteHand);
+// });
